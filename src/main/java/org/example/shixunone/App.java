75ee83d8c39470508b310cad7ead62d06824de28
zengxpmaster <88627703+zengxpmaster@users.noreply.github.com>
2026-04-20 11:35:29 +0800

    initial project

---
+++ src/main/java/org/example/shixunone/App.java
@@ -0,0 +1,10 @@
+package org.example.shixunone;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/example/shixunone/AppTest.java
@@ -0,0 +1,19 @@
+package org.example.shixunone;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
